6a86e1149fda44a4327d893c7e60c9ee8d9cde43
uwflatlander <tschaab@gmail.com>
2016-01-01 22:04:25 -0600

    Hook app into endpoint

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,6 +45,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
+        new JokeFetchTask().execute(new Pair<Context, String>(this, "Manfred"));
+
         JokeProvider jokeProvider = new JokeProvider();
         Intent intent = JokeDisplay.buildIntent(this, jokeProvider.getJoke());
         startActivity(intent);
