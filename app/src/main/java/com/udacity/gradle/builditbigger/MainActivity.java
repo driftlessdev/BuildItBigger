d5db9f9db372ffb4730f22e67b323d6673dc2927
uwflatlander <tschaab@gmail.com>
2016-01-01 18:57:37 -0600

    Launch library activity on button press

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import com.example.humor.JokeProvider;
+import com.testinprod.courtjester.JokeDisplay;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -42,8 +42,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        JokeProvider jokeProvider = new JokeProvider();
-        Toast.makeText(this, jokeProvider.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeDisplay.class);
+        startActivity(intent);
     }
 
 
